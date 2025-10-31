df9754931661869f520857571fc739b80b99817b
nuts-n-bits <nuts-n-bits@outlook.com>
2025-10-31 05:20:29 -0400

    housekeeping

--- src/clearshape/compiler0-inspect-ast.go
+++ src/clearshape/compiler0-inspect-ast.go
@@ -8,3 +8,13 @@ func (ast *AstProgram) DebugString() string {
 	a, _ := json.Marshal(ast)
 	return string(a)
 }
+
+func (flt *FltProgram) DebugString() string {
+	a, _ := json.Marshal(flt)
+	return string(a)
+}
+
+func (lc *LcProgram) DebugString() string{
+	a, _ := json.Marshal(lc)
+	return string(a)
+}
